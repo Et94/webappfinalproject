2c30ee24e77725162bf4e247201a01e627a1b084
Jungmin Harry Kim <beramode0@hotmail.com>
2020-03-27 13:43:21 -0700

    Fixed date format

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,11 @@
 let db = require('../utils/db');
 
 function getAllPosts() {
-	return db.query("SELECT to_char(date, 'Mon DD, YYYY HH:MI PM'), * from posts inner join users ON posts.userid = users.userid ORDER BY posts.date DESC");
+	return db.query("SELECT to_char(date, 'Mon DD, YYYY'), * from posts inner join users ON posts.userid = users.userid ORDER BY posts.date DESC");
 }
 
 function getPostsByUserId(id) {
-	return db.query("SELECT to_char(date, 'Mon DD, YYYY HH:MI PM'), * from posts inner join users ON posts.userid = users.userid where posts.userid = " + id + " ORDER BY posts.date DESC");
+	return db.query("SELECT to_char(date, 'Mon DD, YYYY'), * from posts inner join users ON posts.userid = users.userid where posts.userid = " + id + " ORDER BY posts.date DESC");
 }
 
 module.exports = {
